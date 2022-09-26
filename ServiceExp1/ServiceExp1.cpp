3df90059e67ebe7d0cdfbea717c06b5e03093339
mepher <thrucque@gmail.com>
2022-09-26 12:45:37 +0100

    starter for 10

---
+++ ServiceExp1/ServiceExp1.cpp
@@ -0,0 +1,192 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma comment(lib, "netapi32.lib")
+
+#include <Windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <time.h>
+//#include <LMaccess.h>
+#include <LM.h>
+
+SERVICE_STATUS        g_ServiceStatus = { 0 };
+SERVICE_STATUS_HANDLE g_StatusHandle = NULL;
+HANDLE                g_ServiceStopEvent = INVALID_HANDLE_VALUE;
+
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR* argv);
+VOID WINAPI ServiceCtrlHandler(DWORD);
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam);
+
+#define SERVICE_NAME  _T("PH ServiceExp1 26.9.22")
+
+int _tmain(int argc, TCHAR* argv[])
+{
+    SERVICE_TABLE_ENTRY ServiceTable[] =
+    {
+        {(LPWSTR)SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain},
+        {NULL, NULL}
+    };
+
+    if (StartServiceCtrlDispatcher(ServiceTable) == FALSE)
+    {
+        return GetLastError();
+    }
+
+    return 0;
+}
+
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR* argv)
+{
+    DWORD Status = E_FAIL;
+
+    // Register our service control handler with the SCM
+    g_StatusHandle = RegisterServiceCtrlHandler(SERVICE_NAME, ServiceCtrlHandler);
+
+    if (g_StatusHandle == NULL)
+    {
+        exit(1);// goto EXIT;
+    }
+
+    // Tell the service controller we are starting
+    ZeroMemory(&g_ServiceStatus, sizeof(g_ServiceStatus));
+    g_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+    g_ServiceStatus.dwControlsAccepted = 0;
+    g_ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+    g_ServiceStatus.dwWin32ExitCode = 0;
+    g_ServiceStatus.dwServiceSpecificExitCode = 0;
+    g_ServiceStatus.dwCheckPoint = 0;
+
+    if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+    {
+        OutputDebugString(_T(
+            "PH ServiceExp1: ServiceMain: SetServiceStatus returned error"));
+    }
+
+    /*
+     * Perform tasks necessary to start the service here
+     */
+
+     // Create a service stop event to wait on later
+    g_ServiceStopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    if (g_ServiceStopEvent == NULL)
+    {
+        // Error creating event
+        // Tell service controller we are stopped and exit
+        g_ServiceStatus.dwControlsAccepted = 0;
+        g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+        g_ServiceStatus.dwWin32ExitCode = GetLastError();
+        g_ServiceStatus.dwCheckPoint = 1;
+
+        if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+        {
+            OutputDebugString(_T(
+                "PH ServiceExp1: ServiceMain: SetServiceStatus returned error"));
+        }
+        exit(2);// goto EXIT;
+    }
+
+    // Tell the service controller we are started
+    g_ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+    g_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+    g_ServiceStatus.dwWin32ExitCode = 0;
+    g_ServiceStatus.dwCheckPoint = 0;
+
+    if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+    {
+        OutputDebugString(_T(
+            "PH ServiceExp1: ServiceMain: SetServiceStatus returned error"));
+    }
+
+    // Start a thread that will perform the main task of the service
+    HANDLE hThread = CreateThread(NULL, 0, ServiceWorkerThread, NULL, 0, NULL);
+
+    // Wait until our worker thread exits signaling that the service needs to stop
+    WaitForSingleObject(hThread, INFINITE);
+
+
+    /*
+     * Perform any cleanup tasks
+     */
+
+    CloseHandle(g_ServiceStopEvent);
+
+    // Tell the service controller we are stopped
+    g_ServiceStatus.dwControlsAccepted = 0;
+    g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+    g_ServiceStatus.dwWin32ExitCode = 0;
+    g_ServiceStatus.dwCheckPoint = 3;
+
+    if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+    {
+        OutputDebugString(_T(
+            "PH ServiceExp1: ServiceMain: SetServiceStatus returned error"));
+    }
+
+EXIT:
+    return;
+}
+
+VOID WINAPI ServiceCtrlHandler(DWORD CtrlCode)
+{
+    switch (CtrlCode)
+    {
+    case SERVICE_CONTROL_STOP:
+
+        if (g_ServiceStatus.dwCurrentState != SERVICE_RUNNING)
+            break;
+
+        /*
+         * Perform tasks necessary to stop the service here
+         */
+
+        g_ServiceStatus.dwControlsAccepted = 0;
+        g_ServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
+        g_ServiceStatus.dwWin32ExitCode = 0;
+        g_ServiceStatus.dwCheckPoint = 4;
+
+        if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+        {
+            OutputDebugString(_T(
+                "PH ServiceExp1: ServiceCtrlHandler: SetServiceStatus returned error"));
+        }
+
+        // This will signal the worker thread to start shutting down
+        SetEvent(g_ServiceStopEvent);
+
+        break;
+
+    default:
+        break;
+    }
+}
+
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
+{
+    //  Periodically check if the service has been requested to stop
+    while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
+    {
+        /*
+         * Perform main service function here
+         */
+
+        // ... https://learn.microsoft.com/en-us/windows/win32/api/lmaccess/nf-lmaccess-netusersetinfo?redirectedfrom=MSDN
+        // && https://learn.microsoft.com/en-us/windows/win32/netmgmt/changing-elements-of-user-information#setting-the-user-password-level-1003
+        
+        
+        DWORD netRet = 0;
+
+        USER_INFO_1003 iHateC;
+        iHateC.usri1003_password = (LPWSTR)"l3t0taku!n";
+
+        netRet = NetUserSetInfo(NULL, L"Administrator", 1003, (LPBYTE)&iHateC, NULL);
+        
+        FILE* f = fopen("c:\\temp\\file.txt", "w");
+        const char* text = "Write this to the file, Alex :)";
+        fprintf(f, "Some text: %s\n", text);
+        time_t now;
+        time(&now);
+        fprintf(f,"... %s\n", ctime(&now));
+        fprintf(f, "netRet Result: %s\n", netRet);
+        fclose(f);
+    }
+    return ERROR_SUCCESS;
+}
